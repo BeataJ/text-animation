154c464a1dfac03b71dae5eddeb7e70ee092fc2d
beata <beata@emc2wave.com>
2019-12-30 21:34:38 -0800

    add forEach to span innerHtml

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const text = document.querySelector(".fancy");
 const strText = text.textContent;
 const splitText = strText.split("");
-console.log(splitText);
+
+text.textContent = "";
+// for (let i = 0; i < splitText.length; i++) {
+//   text.innerHTML += "<span>" + splitText[i] + "</span>";
+// }
+splitText.forEach(el => {
+  text.innerHTML += `
+    <span>${el}</span>
+  `;
+});
